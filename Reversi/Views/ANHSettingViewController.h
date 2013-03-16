c20811a764fb15abc2d6f3ad7e3ef5ca68382edc
Anh Nguyen <anh@Anhs-Mac.local>
2013-03-16 02:14:05 -0400

    Added iPhone support

---
+++ Reversi/Views/ANHSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  ANHSettingViewController.h
+//  Reversi
+//
+//  Created by Anh Nguyen on 3/15/13.
+//  Copyright (c) 2013 Anh Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANHSettingViewController : UIViewController
+
+@end
---
+++ Reversi/Views/ANHSettingViewController.m
@@ -0,0 +1,38 @@
+//
+//  ANHSettingViewController.m
+//  Reversi
+//
+//  Created by Anh Nguyen on 3/15/13.
+//  Copyright (c) 2013 Anh Nguyen. All rights reserved.
+//
+
+#import "ANHSettingViewController.h"
+
+@interface ANHSettingViewController ()
+
+@end
+
+@implementation ANHSettingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
